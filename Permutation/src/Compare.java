87d8268f7b1644bdfca5c6d22673567f90108b0d
samir <samir@LAPTOP-FQ073QM0>
2016-12-23 12:31:06 -0500

    Final commit

--- Permutation/src/Compare.java
+++ Permutation/src/Compare.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Compare {
@@ -6,29 +7,55 @@ public class Compare {
 		// TODO Auto-generated method stub
 		boolean result;		
 		String str1="",str2="";
-result = compareStrings(str1,str2);
-	}
-
-	private static boolean compareStrings(String str1, String str2) {
-		// TODO Auto-generated method stub
-		
-		takeInput();
-		System.out.println(str1);
-		System.out.println(str2);
-		
-		return false;
-	}
-
-	private static void takeInput() {
-		// TODO Auto-generated method stub
+		//takeInput();
 		System.out.println("Enter String 1");
-Scanner obj1 = new Scanner(System.in);
-String str1=obj1.next();
+		Scanner obj1 = new Scanner(System.in);
+		 str1=obj1.next();
 
-		System.out.println("Enter String 1");
+		System.out.println("Enter String 2");
 		Scanner obj2 = new Scanner(System.in);
-String str2=obj2.next();
-compareStrings(Str1,str2);
+		 str2=obj2.next();
+		//compareStrings(str1,str2);
+		
+		result = compareStrings(str1,str2);
+		System.out.println(result);
 	}
+
+	private static boolean compareStrings(String s1, String s2) {
+		// TODO Auto-generated method stub
+
+		 HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		    int count =1;
+		        if(s1.length()!=s2.length())
+		        {
+		            return false;
+		        }
+		            for(Character c: s1.toCharArray())
+		            {
+		                if(!map.containsKey(c))
+		                    map.put(c, count);
+
+		                else
+		                    map.put(c, count+1);
+
+		            }
+		            for(Character c: s2.toCharArray())
+		            {
+		                if(!map.containsKey(c))
+		                    return false;
+
+		                else
+		                    map.put(c, count-1);
+		            }
+		            for(Character c: map.keySet())
+		            {
+		                if(map.get(c)!=0)
+		                    return false;
+		            }
+		        return true;
+
+	}
+
+	
 
 }
